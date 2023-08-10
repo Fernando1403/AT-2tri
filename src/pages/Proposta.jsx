f7039ba18f04a43130aa10eecdd0dddedcf26a1d
Fernando <84139372+Fernando1403@users.noreply.github.com>
2023-08-10 13:34:12 -0300

    Add files via upload

--- src/pages/Proposta.jsx
+++ src/pages/Proposta.jsx
@@ -5,7 +5,7 @@ function Proposta() {
     <>
       <div className="d-flex">
         <div className="text-container">
-          <h3 style={{ marginLeft: "70px" }}>Nossa Proposta</h3>
+          <h1 style={{fontSize: "50px", marginLeft: "70px", margin:'70px' }}>Nossa Proposta</h1>
           <p style={{ fontSize: "30px", width: "60%", marginLeft: "70px" }}>
             A Wild Life Protection é uma ONG dedicada à conservação da vida
             selvagem e seus habitats. Atuamos por meio de projetos de
@@ -23,6 +23,7 @@ function Proposta() {
               naxHeight:'700px',
               borderRadius: "10px",
               marginLeft: "-12rem",
+              marginTop:'5rem'
             }}
           />
         </div>
